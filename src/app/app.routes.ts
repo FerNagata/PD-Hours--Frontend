c144fb8796ccf93c764ef818df9677e76e5951bc
FerNagata <fer.nagataito@gmail.com>
2025-01-25 16:20:59 -0300

    feat: added base components and configurations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SquadDataComponent } from './components/squad-data/squad-data.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: HomeComponent,
+    }, 
+    {
+        path: 'squad-data',
+        component: SquadDataComponent,
+    }, 
+];
